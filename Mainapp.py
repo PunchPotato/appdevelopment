d73609ee5acd14fa9e000846c63927892fc7791d
PunchPotato <joshuaharrison04@outlook.com>
2023-08-02 15:13:16 +0100

    Main app GUI design

--- Mainapp.py
+++ Mainapp.py
@@ -15,27 +15,81 @@ class Page1(tk.Frame):
         main_title = tk.Label(self, text='Everything Fitness', font=self.custom_font, bg='#b3b5ba', fg='#000000')
         main_title.place(y=10, x=30)
 
+        self.calorie_button = ImageTk.PhotoImage(Image.open('Login page/cals button.png'))
+        self.weight_button = ImageTk.PhotoImage(Image.open('Login page/weight button.png'))
+        self.profile_button = ImageTk.PhotoImage(Image.open('Login page/profile button.png'))
 
-        label = tk.Label(self, text="This is Page 1")
-        label.pack(pady=200, padx=10)
-        
-        button = tk.Button(self, text="Go to Page 2", command=lambda: self.controller.show_frame(Page2))
-        button.pack()
+        button = tk.Button(self, image=self.calorie_button, bd=0, bg='#b3b5ba', activebackground='#b3b5ba',
+                            cursor='hand2', command=lambda: self.controller.show_frame(Page1))
+        button.place(y=880, x=20)
+
+        button = tk.Button(self, image=self.weight_button,bd=0, bg='#b3b5ba', activebackground='#b3b5ba',
+                            command=lambda: self.controller.show_frame(Page2))
+        button.place(y= 880, x = 300)
+
+        button = tk.Button(self, image=self.profile_button, bd=0, bg='#b3b5ba', activebackground='#b3b5ba',
+                            cursor='hand2', command=lambda: self.controller.show_frame(Page3))
+        button.place(y=880, x=600)
 
 class Page2(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        
+        self.custom_font = font.Font(family="typewriter", size=60, weight="normal")
+
         self.bg_image = ImageTk.PhotoImage(Image.open("Login page/Main app.png"))
         self.bg_label = tk.Label(self, image=self.bg_image)
         self.bg_label.place(y=0, x=0)
 
-        label = tk.Label(self, text="This is Page 2")
-        label.pack(pady=200, padx=10)
-        
-        button = tk.Button(self, text="Go to Page 1", command=lambda: self.controller.show_frame(Page1))
-        button.pack()
+        main_title = tk.Label(self, text='Everything Fitness', font=self.custom_font, bg='#b3b5ba', fg='#000000')
+        main_title.place(y=10, x=30)
+
+        self.calorie_button = ImageTk.PhotoImage(Image.open('Login page/cals button.png'))
+        self.weight_button = ImageTk.PhotoImage(Image.open('Login page/weight button.png'))
+        self.profile_button = ImageTk.PhotoImage(Image.open('Login page/profile button.png'))
+
+        button = tk.Button(self, image=self.calorie_button, bd=0, bg='#b3b5ba', activebackground='#b3b5ba',
+                            cursor='hand2', command=lambda: self.controller.show_frame(Page1))
+        button.place(y=880, x=20)
+
+        button = tk.Button(self, image=self.weight_button,bd=0, bg='#b3b5ba', activebackground='#b3b5ba',
+                            command=lambda: self.controller.show_frame(Page2))
+        button.place(y= 880, x = 300)
+
+        button = tk.Button(self, image=self.profile_button, bd=0, bg='#b3b5ba', activebackground='#b3b5ba',
+                            cursor='hand2', command=lambda: self.controller.show_frame(Page3))
+        button.place(y=880, x=600)
+
+class Page3(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.custom_font = font.Font(family="typewriter", size=60, weight="normal")
+
+        self.bg_image = ImageTk.PhotoImage(Image.open("Login page/Main app.png"))
+        self.bg_label = tk.Label(self, image=self.bg_image)
+        self.bg_label.place(y=0, x=0)
+
+        main_title = tk.Label(self, text='Everything Fitness', font=self.custom_font, bg='#b3b5ba', fg='#000000')
+        main_title.place(y=10, x=30)
+
+        self.calorie_button = ImageTk.PhotoImage(Image.open('Login page/cals button.png'))
+        self.weight_button = ImageTk.PhotoImage(Image.open('Login page/weight button.png'))
+        self.profile_button = ImageTk.PhotoImage(Image.open('Login page/profile button.png'))
+
+        button = tk.Button(self, image=self.calorie_button, bd=0, bg='#b3b5ba', activebackground='#b3b5ba',
+                            cursor='hand2', command=lambda: self.controller.show_frame(Page1))
+        button.place(y=880, x=20)
+
+        button = tk.Button(self, image=self.weight_button,bd=0, bg='#b3b5ba', activebackground='#b3b5ba',
+                            command=lambda: self.controller.show_frame(Page2))
+        button.place(y= 880, x = 300)
+
+        button = tk.Button(self, image=self.profile_button, bd=0, bg='#b3b5ba', activebackground='#b3b5ba',
+                            cursor='hand2', command=lambda: self.controller.show_frame(Page3))
+        button.place(y=880, x=600)
+
+
 
 class MainApp(tk.Tk):
     def __init__(self):
@@ -48,7 +102,7 @@ class MainApp(tk.Tk):
         self.geometry('720x980')
         
         self.frames = {}
-        for F in (Page1, Page2):
+        for F in (Page1, Page2, Page3):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")

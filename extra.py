d8c80d873131105799c606a6289b5b8d61074d50
PunchPotato <joshuaharrison04@outlook.com>
2023-08-26 10:35:27 +0100

    outlined boxes

--- extra.py
+++ extra.py
@@ -1,32 +1,27 @@
+
 import tkinter as tk
-
-class FoodApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Food App")
-        self.buttons = []  # List to store the created buttons
-        self.current_y = 240
-        
-        self.frame = tk.Frame(root, bg='#b3b5ba')
-        self.frame.pack(fill=tk.BOTH, expand=True)
-        
-        self.add_food_button = tk.Button(self.frame, text="Add Food", command=self.add_food)
-        self.add_food_button.place(y=300, x=200)
-    
-    def add_food(self):
-        name = "Food Name"  # Replace with the actual name of the food
-        calories = 100  # Replace with the actual calorie content
-        
-        new_button = tk.Button(self.frame, text="", font=("typewriter", 20, "normal"), bg='#b3b5ba', bd=0,
-                               command=lambda: self.controller.show_frame(FoodInfoPage))
-        
-        if self.buttons:
-            self.current_y += 100
-        new_button.place(y=self.current_y, x=200)
-        new_button.config(text=f"{name}, {calories} cals".title())
-        self.buttons.append(new_button)
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = FoodApp(root)
-    root.mainloop()
+  
+root = tk.Tk()
+root.geometry('250x150')
+root.title("Button Border")
+  
+# Label
+l = tk.Label(root, text = "Enter your Roll No. :",
+             font = (("Times New Roman"), 15))
+l.pack()
+  
+# Entry Widget
+tk.Entry(root).pack()
+  
+# for space between widgets
+tk.Label(root, text=" ").pack()
+  
+# Frame for button border with black border color
+button_border = tk.Frame(root, highlightbackground = "black", 
+                         highlightthickness = 2, bd=0)
+bttn = tk.Button(button_border, text = 'Submit', fg = 'black',
+                 bg = 'yellow',font = (("Times New Roman"),15))
+bttn.pack()
+button_border.pack()
+  
+root.mainloop()
